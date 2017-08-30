164b1b8e1f64993898e8bb0fc3b75ac62177fc95
Brian Flannery <brian@union-pos.com>
2017-08-30 12:51:00 -0500

    WIP: toggleAssertion test passing; need to refactor

--- test/specs/user/pageobjects/pages/inventory.page.js
+++ test/specs/user/pageobjects/pages/inventory.page.js
@@ -1,3 +1,4 @@
+const assert = require('assert')
 const Page = require('./page')
 
 class InventoryPage extends Page {
@@ -9,23 +10,87 @@ class InventoryPage extends Page {
 	get allStatusButton() {
 		return browser.element('div.btn-group button:first-child')
 	}
+	
 	get activeStatusButton() {
 		return browser.element('div.btn-group button:nth-child(2)')
 	}
+	
 	get inactiveStatusButton() {
 		return browser.element('.btn-group button:nth-child(3)')
 	}
+	
 	get pendingStatusButton() {
 		return browser.element('.btn-group button:nth-child(4)')
 	}
+	
+	get table() {
+		return browser.element('table')
+	}
+	
+	get tableHeader() {
+		return browser.element('table thead')
+	}
+	
+	get tableHeaderRow() {
+		return browser.element('table thead tr')
+	}
+	
+	get tableHeaderRowColumns() {
+		return browser.element('table thead tr th')
+	}
+	
+	get tableBody() {
+		return browser.element('table tbody')
+	}
+	
 	get tableRowItem() {
 		return browser.element('table tbody tr')
 	}
-	get tableActionButton() {
-		return browser.element('.actions div a:nth-child(3)')
+	
+	get tableRowItemStatus() {
+		return browser.element('table tbody tr a[class="item-state"]')
 	}
+	
 	open() {
 		super.open('/inventory')
+	}
+	
+	clickItem(item) {
+		item.click()
+	}
+	
+	statusColorAssertion(selector, color) {
+		this.clickItem(selector)
+		this.tableBody.waitForExist()
+		const rows = browser.elements('table tbody tr a[class="item-state"]')
+		if (rows) {
+			return rows.value.forEach((row) => {
+				assert.equal(row.getCssProperty('color').value, color)
+			})
+		}
+		assert.equal(rows, 0)
+	}
+	
+	toggleColumnsAssertion() {
+		const allColumns = browser.elements('table thead tr th')
+		allColumns.value.forEach((singleColumn) => {
+			if (singleColumn.getAttribute('class') !== 'sorting_disabled') {
+				if(singleColumn.getAttribute('aria-label').includes('Display Name')) {
+					assert.equal(singleColumn.getAttribute('class'), 'sorting_asc')
+					assert.equal(singleColumn.getAttribute('aria-sort'), 'ascending')
+					singleColumn.click()
+					assert.equal(singleColumn.getAttribute('class'), 'sorting_desc')
+					assert.equal(singleColumn.getAttribute('aria-sort'), 'descending')
+				}
+				
+				singleColumn.click()
+				assert.equal(singleColumn.getAttribute('class'), 'sorting_asc')
+				assert.equal(singleColumn.getAttribute('aria-sort'), 'ascending')
+				singleColumn.click()
+				assert.equal(singleColumn.getAttribute('class'), 'sorting_desc')
+				assert.equal(singleColumn.getAttribute('aria-sort'), 'descending')
+			}
+		})
 	}
 }
 
--- test/specs/user/pages/inventory/inventory.spec.js
+++ test/specs/user/pages/inventory/inventory.spec.js
@@ -8,57 +8,40 @@ describe('Union Portal Inventory page', function () {
 		browser.waitForVisible('#topnav')
 		browser.waitForVisible('.navbar-custom')
 		InventoryPage.open()
-		InventoryPage.statusButtonsContainer.waitForExist()
-		
+		InventoryPage.table.waitForExist()
 	})
-	// it('should navigate to the inventory page url', () => {
-	// 	const url = browser.getUrl()
-	// 	assert.equal(url, InventoryPage.getBaseUrl().concat('inventory'))
-	// })
-	it('should display all items when page is loaded', () => {
-		browser.waitForExist('table tbody')
-		const allRows = browser.elements('a[class="item-state"]')
-		if(allRows) {
-			return allRows.value.forEach((allRow) => {
-				assert.equal((
-					allRow.getCssProperty('color').value === 'rgba(37,172,90,1)' ||
-					allRow.getCssProperty('color').value === 'rgba(238,28,37,1)' ||
-					allRow.getCssProperty('color').value === 'rgba(216,200,44,1)'), true)
-			})
-		}
-		assert.equal(allRows, 0)
+	describe('Item Status', () => {
+		it('should navigate to the inventory page url', () => {
+			const url = browser.getUrl()
+			assert.equal(url, InventoryPage.getBaseUrl().concat('inventory'))
+		})
+		it('should display all items when page is loaded', () => {
+			const allRows = InventoryPage.tableRowItemStatus.elements()
+			if(allRows) {
+				return allRows.value.forEach((allRow) => {
+					assert.equal((
+						allRow.getCssProperty('color').value === 'rgba(37,172,90,1)' ||
+						allRow.getCssProperty('color').value === 'rgba(238,28,37,1)' ||
+						allRow.getCssProperty('color').value === 'rgba(216,200,44,1)'), true)
+				})
+			}
+			assert.equal(allRows, 0)
+		})
+		it('should only display only active inventory items when active is clicked', () => {
+			InventoryPage.statusColorAssertion(InventoryPage.activeStatusButton, 'rgba(37,172,90,1)')
+		})
+		it('should only display inactive inventory items when inactive is clicked', () => {
+			InventoryPage.statusColorAssertion(InventoryPage.inactiveStatusButton, 'rgba(238,28,37,1)')
+		})
+		it('should only display pending inventory items when pending is clicked', () => {
+			InventoryPage.statusColorAssertion(InventoryPage.pendingStatusButton, 'rgba(216,200,44,1)')
+		})
 	})
-	it('should only display only active inventory items when active is clicked', () => {
-		InventoryPage.activeStatusButton.click()
-		browser.waitForExist('table tbody')
-		const activeRows = browser.elements('a[class="item-state"]')
-		if(activeRows) {
-			return activeRows.value.forEach((activeRow) => {
-				assert.equal(activeRow.getCssProperty('color').value, 'rgba(37,172,90,1)')
-			})
-		}
-		assert.equal(activeRows, 0)
+	
+	describe('Column Toggle' , () => {
+		it.only('should toggle the order of items when header column is clicked', () => {
+			InventoryPage.toggleColumnsAssertion()
+		})
 	})
-	it('should only display inactive inventory items when inactive is clicked', () => {
-		InventoryPage.inactiveStatusButton.click()
-		browser.waitForExist('table tbody')
-		const inactiveRows = browser.elements('a[class="item-state"]')
-		if(inactiveRows) {
-			return inactiveRows.value.forEach((inactiveRow) => {
-				assert.equal(inactiveRow.getCssProperty('color').value, 'rgba(238,28,37,1)')
-			})
-		}
-		assert.equal(inactiveRows, 0)
-	})
-	it('should only display pending inventory items when pending is clicked', () => {
-		InventoryPage.pendingStatusButton.click()
-		browser.waitForExist('table tbody')
-		const pendingRows = browser.elements('a[class="item-state"]')
-		if(pendingRows) {
-			return pendingRows.value.forEach((pendingRow) => {
-				assert.equal(pendingRow.getCssProperty('color').value, 'rgba(216,200,44,1)')
-			})
-		}
-		assert.equal(pendingRows, 0, 'pendingRows are zero')
-	})
+	
 })
